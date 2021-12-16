f246de509e93f14d330726545c27d14eb60413ca
unknown <henri11silvafcc@gmail.com>
2021-12-16 15:52:12 +0000

    Refactored example 5

--- refactoring/src/main/java/pt/up/fe/ldts/example5/Turtle.java
+++ refactoring/src/main/java/pt/up/fe/ldts/example5/Turtle.java
@@ -23,22 +23,92 @@ public class Turtle {
         return direction;
     }
 
+    public void setDirectionNorth(){
+        direction = 'N';
+    }
+
+    public void setDirectionSouth(){
+        direction = 'S';
+    }
+
+    public void setDirectionEast(){
+        direction = 'E';
+    }
+
+    public void setDirectionWest(){
+        direction = 'W';
+    }
+
+    public void executeNorth(char command){
+        switch(command){
+            case 'L':
+                setDirectionWest();
+                break;
+            case 'R':
+                setDirectionEast();
+                break;
+            case 'F':
+                row--;
+                break;
+        }
+    }
+
+    public void executeSouth(char command){
+        switch(command){
+            case 'L':
+                setDirectionEast();
+                break;
+            case 'R':
+                setDirectionWest();
+                break;
+            case 'F':
+                row++;
+                break;
+        }
+    }
+
+    public void executeWest(char command){
+        switch(command){
+            case 'L':
+                setDirectionSouth();
+                break;
+            case 'R':
+                setDirectionNorth();
+                break;
+            case 'F':
+                column--;
+                break;
+        }
+    }
+
+    public void executeEast(char command){
+        switch(command){
+            case 'L':
+                setDirectionNorth();
+                break;
+            case 'R':
+                setDirectionSouth();
+                break;
+            case 'F':
+                column++;
+                break;
+        }
+    }
+
     public void execute(char command) {
-        if (command == 'L') { // ROTATE LEFT
-            if (direction == 'N') direction = 'W';
-            else if (direction == 'W') direction = 'S';
-            else if (direction == 'S') direction = 'E';
-            else if (direction == 'E') direction = 'N';
-        } else if (command == 'R') { // ROTATE RIGHT
-            if (direction == 'N') direction = 'E';
-            else if (direction == 'E') direction = 'S';
-            else if (direction == 'S') direction = 'W';
-            else if (direction == 'W') direction = 'N';
-        } else if (command == 'F'){ // MOVE FORWARD
-            if (direction == 'N') row--;
-            if (direction == 'S') row++;
-            if (direction == 'W') column--;
-            if (direction == 'E') column++;
+        switch (direction){
+            case 'N':
+                executeNorth(command);
+                break;
+            case 'S':
+                executeSouth(command);
+                break;
+            case 'W':
+                executeWest(command);
+                break;
+            case 'E':
+                executeEast(command);
+                break;
         }
     }
 }
